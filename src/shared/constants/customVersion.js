a1c5504dcd92688ac34787f1f2bc942f958c4df8
huanvo25 <huanvo25@users.noreply.github.com>
2026-07-01 18:18:25 +0700

    chore(version): bump deploy counter

--- src/shared/constants/customVersion.js
+++ src/shared/constants/customVersion.js
@@ -1,6 +1,6 @@
 export const CUSTOM_VERSION_INFO = {
   date: "01/07/2026",
-  deployCount: 5,
+  deployCount: 6,
 };
 
 export const CUSTOM_VERSION = `${CUSTOM_VERSION_INFO.date}.${CUSTOM_VERSION_INFO.deployCount}`;
